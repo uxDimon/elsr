a6aaa88d9d004d80033f683633af95de86baf1ef
Shelkov Dmitriy <bobyorrr@gmail.com>
2020-01-28 13:48:12 +0300

    Добавил слайдер

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import "./styles.scss";
 import Swiper from "swiper";
 import "../node_modules/swiper/css/swiper.min.css";
 
-var mySwiper = new Swiper(".swiper1", {
+var mySwiper = new Swiper(".head-slider > .swiper-container", {
 	// https://swiperjs.com/api
 	// Optional parameters
 	loop: true,
+	effect: "fade",
+	speed: 500,
 
-	// If we need pagination
-	pagination: {
-		el: ".swiper-pagination"
+	fadeEffect: {
+		crossFade: false
+	},
+
+	autoplay: {
+		delay: 5000
 	},
 
 	// Navigation arrows
 	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev"
+		nextEl: ".head-slider__button_next",
+		prevEl: ".head-slider__button_prev"
 	}
 });
