3928052ffba3fd6c078a37ce86c4e148d1b531d5
Shelkov Dmitriy <bobyorrr@gmail.com>
2020-01-31 12:49:32 +0300

    Блок отзывы

--- src/index.js
+++ src/index.js
@@ -23,3 +23,26 @@ var mySwiper = new Swiper(".head-slider > .swiper-container", {
 		prevEl: ".head-slider__button_prev"
 	}
 });
+
+var swiper1 = new Swiper(".review-block__swiper-container_1 > .swiper-container", {
+	// Optional parameters
+	slidesPerView: 4,
+	spaceBetween: 20,
+	slidesPerGroup: 1,
+
+	navigation: {
+		nextEl: ".review-block__swiper-container_1 > .review-block__button_next",
+		prevEl: ".review-block__swiper-container_1 > .review-block__button_prev"
+	}
+});
+var swiper2 = new Swiper(".review-block__swiper-container_2 > .swiper-container", {
+	// Optional parameters
+	slidesPerView: 4,
+	spaceBetween: 20,
+	slidesPerGroup: 1,
+
+	navigation: {
+		nextEl: ".review-block__swiper-container_2 > .review-block__button_next",
+		prevEl: ".review-block__swiper-container_2 > .review-block__button_prev"
+	}
+});
